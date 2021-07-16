1918e7dee6962b37eab607beb3d56a4866f1c30a
Brian Smith <brian.bubbam@gmail.com>
2021-07-16 12:25:43 -0500

    commit tree 034e905f441782f11395daff28a10632b0265a5a

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+# binding "pry"
 
+class Book
+    attr_accessor :author, :genre, :page_count 
+    #attr_accessor both reads and writes
+    #attr_reader only reads/fetches the data to read
+    #attr_wrtire only writes
+    attr_reader :title
 
+    def initialize(title) #initialize will allow us to get the title and set it
+        @title = title
+    end
+
+    def turn_page #setting up a method to be called book.turn_page and then puts out a string
+        puts "Flipping the page...wow, you read fast!"
+    end
+    
+end
